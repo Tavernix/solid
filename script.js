91ee30c666b3ebf818730b82fc6b3ac6f35a3fa7
Tavernix <107910112+Tavernix@users.noreply.github.com>
2025-03-25 16:30:53 +0900

    js all scripts

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 // slider best-card
 let sliders = document.querySelectorAll('.card-best__img-slide'); // нашёл все слайдеры на странице
 for (let i = 0; i < sliders.length; i++) { // на каждый слайдер
@@ -31,7 +30,6 @@ for (let i = 0; i < sliders.length; i++) { // на каждый слайдер
             }
             updateSlider();
         });
-
         updateSlider();
     })();
 }
@@ -39,7 +37,66 @@ for (let i = 0; i < sliders.length; i++) { // на каждый слайдер
 
 
 //hidden cards
-
-let collapse = document.querySelector('.selection__collapse');
+let collapseButton = document.querySelector('.selection__collapse');
 let collapseFlag = false;
-let cards = document.querySelector('.cards');
+let cards = document.querySelectorAll('.card-min');
+let btnExpand = document.querySelector('.card-min__expand');
+
+function updateCardVisibility() {
+    const mediaQuery1024 = window.matchMedia('(max-width: 1024px)');
+    const mediaQuery600 = window.matchMedia('(max-width: 600px)');
+
+    cards.forEach(card => card.classList.add('hidden')); // Скрываем все карточки по умолчанию
+	btnExpand.classList.remove('hidden');
+
+    if (mediaQuery600.matches) {
+        // Если ширина экрана меньше 600px, показываем только первые 3 карточек
+        for (let i = 0; i < 3; i++) {
+            if (cards[i]) {
+                cards[i].classList.remove('hidden');
+            }
+        }
+    } else if (mediaQuery1024.matches) {
+        // Если ширина экрана меньше 1024px, показываем только первые 6 карточек
+        for (let i = 0; i < 6; i++) {
+            if (cards[i]) {
+                cards[i].classList.remove('hidden');
+            }
+        }
+    } else {
+        // Если ширина экрана больше 1024px, показываем все карточки
+        cards.forEach(card => card.classList.remove('hidden'));
+		btnExpand.classList.add('hidden');		
+    }
+
+}
+updateCardVisibility();
+window.addEventListener('resize', updateCardVisibility);
+
+function showAllCards() {
+    cards.forEach(card => card.classList.remove('hidden')); // Показываем все карточки
+    btnExpand.classList.add('hidden'); // Скрываем кнопку
+	console.log('показать все машины');
+}
+btnExpand.addEventListener('click', showAllCards);
+
+
+function toggleButton() {
+    if (collapseFlag) {
+        // Если список свернут
+        collapseButton.textContent = "Свернуть список машин";
+        collapseButton.style.color = "var(--primary-color)";
+        collapseButton.style.backgroundColor = "#FFFFFF";
+		document.querySelector('.cards').classList.remove('hidden');
+    } else {
+        // Если список развернут
+        collapseButton.textContent = "Развернуть список машин";
+        collapseButton.style.color = "#FFFFFF";
+        collapseButton.style.backgroundColor = "var(--primary-color)";
+		document.querySelector('.cards').classList.add('hidden');		
+    }
+    // Меняем состояние
+    collapseFlag = !collapseFlag;
+}
+collapseButton.addEventListener('click', toggleButton);
+
